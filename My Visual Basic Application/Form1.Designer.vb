23630ad78f679e104fc13f260c362051861bda32
Edward Garcia <edward_12_1998@yahoo.com>
2022-07-19 17:47:05 +0800

    Added initial codebase

---
+++ My Visual Basic Application/Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.tbTextArea = New System.Windows.Forms.TextBox()
+        Me.btnNum1 = New System.Windows.Forms.Button()
+        Me.btnNum2 = New System.Windows.Forms.Button()
+        Me.btnNum3 = New System.Windows.Forms.Button()
+        Me.btnNum4 = New System.Windows.Forms.Button()
+        Me.btnNum5 = New System.Windows.Forms.Button()
+        Me.btnNum6 = New System.Windows.Forms.Button()
+        Me.btnNum7 = New System.Windows.Forms.Button()
+        Me.btnNum8 = New System.Windows.Forms.Button()
+        Me.btnNum9 = New System.Windows.Forms.Button()
+        Me.btnNum0 = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnAnswer = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.btnMultiplication = New System.Windows.Forms.Button()
+        Me.btnSubtraction = New System.Windows.Forms.Button()
+        Me.btnAddition = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tbTextArea
+        '
+        Me.tbTextArea.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.tbTextArea.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.tbTextArea.Location = New System.Drawing.Point(12, 12)
+        Me.tbTextArea.Multiline = True
+        Me.tbTextArea.Name = "tbTextArea"
+        Me.tbTextArea.ReadOnly = True
+        Me.tbTextArea.Size = New System.Drawing.Size(230, 51)
+        Me.tbTextArea.TabIndex = 1
+        Me.tbTextArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnNum1
+        '
+        Me.btnNum1.Location = New System.Drawing.Point(12, 69)
+        Me.btnNum1.Name = "btnNum1"
+        Me.btnNum1.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum1.TabIndex = 2
+        Me.btnNum1.Text = "1"
+        Me.btnNum1.UseVisualStyleBackColor = True
+        '
+        'btnNum2
+        '
+        Me.btnNum2.Location = New System.Drawing.Point(71, 69)
+        Me.btnNum2.Name = "btnNum2"
+        Me.btnNum2.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum2.TabIndex = 3
+        Me.btnNum2.Text = "2"
+        Me.btnNum2.UseVisualStyleBackColor = True
+        '
+        'btnNum3
+        '
+        Me.btnNum3.Location = New System.Drawing.Point(130, 69)
+        Me.btnNum3.Name = "btnNum3"
+        Me.btnNum3.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum3.TabIndex = 4
+        Me.btnNum3.Text = "3"
+        Me.btnNum3.UseVisualStyleBackColor = True
+        '
+        'btnNum4
+        '
+        Me.btnNum4.Location = New System.Drawing.Point(12, 125)
+        Me.btnNum4.Name = "btnNum4"
+        Me.btnNum4.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum4.TabIndex = 5
+        Me.btnNum4.Text = "4"
+        Me.btnNum4.UseVisualStyleBackColor = True
+        '
+        'btnNum5
+        '
+        Me.btnNum5.Location = New System.Drawing.Point(71, 125)
+        Me.btnNum5.Name = "btnNum5"
+        Me.btnNum5.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum5.TabIndex = 6
+        Me.btnNum5.Text = "5"
+        Me.btnNum5.UseVisualStyleBackColor = True
+        '
+        'btnNum6
+        '
+        Me.btnNum6.Location = New System.Drawing.Point(130, 125)
+        Me.btnNum6.Name = "btnNum6"
+        Me.btnNum6.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum6.TabIndex = 7
+        Me.btnNum6.Text = "6"
+        Me.btnNum6.UseVisualStyleBackColor = True
+        '
+        'btnNum7
+        '
+        Me.btnNum7.Location = New System.Drawing.Point(12, 181)
+        Me.btnNum7.Name = "btnNum7"
+        Me.btnNum7.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum7.TabIndex = 8
+        Me.btnNum7.Text = "7"
+        Me.btnNum7.UseVisualStyleBackColor = True
+        '
+        'btnNum8
+        '
+        Me.btnNum8.Location = New System.Drawing.Point(71, 181)
+        Me.btnNum8.Name = "btnNum8"
+        Me.btnNum8.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum8.TabIndex = 9
+        Me.btnNum8.Text = "8"
+        Me.btnNum8.UseVisualStyleBackColor = True
+        '
+        'btnNum9
+        '
+        Me.btnNum9.Location = New System.Drawing.Point(130, 181)
+        Me.btnNum9.Name = "btnNum9"
+        Me.btnNum9.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum9.TabIndex = 10
+        Me.btnNum9.Text = "9"
+        Me.btnNum9.UseVisualStyleBackColor = True
+        '
+        'btnNum0
+        '
+        Me.btnNum0.Location = New System.Drawing.Point(71, 237)
+        Me.btnNum0.Name = "btnNum0"
+        Me.btnNum0.Size = New System.Drawing.Size(53, 50)
+        Me.btnNum0.TabIndex = 11
+        Me.btnNum0.Text = "0"
+        Me.btnNum0.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(12, 237)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(53, 50)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "C"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnAnswer
+        '
+        Me.btnAnswer.Location = New System.Drawing.Point(130, 237)
+        Me.btnAnswer.Name = "btnAnswer"
+        Me.btnAnswer.Size = New System.Drawing.Size(53, 50)
+        Me.btnAnswer.TabIndex = 13
+        Me.btnAnswer.Text = "="
+        Me.btnAnswer.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.Location = New System.Drawing.Point(189, 237)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(53, 50)
+        Me.btnDivision.TabIndex = 17
+        Me.btnDivision.Text = "/"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'btnMultiplication
+        '
+        Me.btnMultiplication.Location = New System.Drawing.Point(189, 181)
+        Me.btnMultiplication.Name = "btnMultiplication"
+        Me.btnMultiplication.Size = New System.Drawing.Size(53, 50)
+        Me.btnMultiplication.TabIndex = 16
+        Me.btnMultiplication.Text = "*"
+        Me.btnMultiplication.UseVisualStyleBackColor = True
+        '
+        'btnSubtraction
+        '
+        Me.btnSubtraction.Location = New System.Drawing.Point(189, 125)
+        Me.btnSubtraction.Name = "btnSubtraction"
+        Me.btnSubtraction.Size = New System.Drawing.Size(53, 50)
+        Me.btnSubtraction.TabIndex = 15
+        Me.btnSubtraction.Text = "-"
+        Me.btnSubtraction.UseVisualStyleBackColor = True
+        '
+        'btnAddition
+        '
+        Me.btnAddition.Location = New System.Drawing.Point(189, 69)
+        Me.btnAddition.Name = "btnAddition"
+        Me.btnAddition.Size = New System.Drawing.Size(53, 50)
+        Me.btnAddition.TabIndex = 14
+        Me.btnAddition.Text = "+"
+        Me.btnAddition.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(257, 298)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnMultiplication)
+        Me.Controls.Add(Me.btnSubtraction)
+        Me.Controls.Add(Me.btnAddition)
+        Me.Controls.Add(Me.btnAnswer)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnNum0)
+        Me.Controls.Add(Me.btnNum9)
+        Me.Controls.Add(Me.btnNum8)
+        Me.Controls.Add(Me.btnNum7)
+        Me.Controls.Add(Me.btnNum6)
+        Me.Controls.Add(Me.btnNum5)
+        Me.Controls.Add(Me.btnNum4)
+        Me.Controls.Add(Me.btnNum3)
+        Me.Controls.Add(Me.btnNum2)
+        Me.Controls.Add(Me.btnNum1)
+        Me.Controls.Add(Me.tbTextArea)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator Application"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tbTextArea As TextBox
+    Friend WithEvents btnNum1 As Button
+    Friend WithEvents btnNum2 As Button
+    Friend WithEvents btnNum3 As Button
+    Friend WithEvents btnNum4 As Button
+    Friend WithEvents btnNum5 As Button
+    Friend WithEvents btnNum6 As Button
+    Friend WithEvents btnNum7 As Button
+    Friend WithEvents btnNum8 As Button
+    Friend WithEvents btnNum9 As Button
+    Friend WithEvents btnNum0 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnAnswer As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnMultiplication As Button
+    Friend WithEvents btnSubtraction As Button
+    Friend WithEvents btnAddition As Button
+End Class
---
+++ My Visual Basic Application/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+
+    Dim initialValue As Integer
+    Dim userOperator As String
+
+    Private Sub btnNum1_Click(sender As Object, e As EventArgs) Handles btnNum1.Click
+        tbTextArea.Text += "1"
+    End Sub
+
+    Private Sub btnNum2_Click(sender As Object, e As EventArgs) Handles btnNum2.Click
+        tbTextArea.Text += "2"
+    End Sub
+
+    Private Sub btnNum3_Click(sender As Object, e As EventArgs) Handles btnNum3.Click
+        tbTextArea.Text += "3"
+    End Sub
+
+    Private Sub btnNum4_Click(sender As Object, e As EventArgs) Handles btnNum4.Click
+        tbTextArea.Text += "4"
+    End Sub
+
+    Private Sub btnNum5_Click(sender As Object, e As EventArgs) Handles btnNum5.Click
+        tbTextArea.Text += "5"
+    End Sub
+
+    Private Sub btnNum6_Click(sender As Object, e As EventArgs) Handles btnNum6.Click
+        tbTextArea.Text += "6"
+    End Sub
+
+    Private Sub btnNum7_Click(sender As Object, e As EventArgs) Handles btnNum7.Click
+        tbTextArea.Text += "7"
+    End Sub
+
+    Private Sub btnNum8_Click(sender As Object, e As EventArgs) Handles btnNum8.Click
+        tbTextArea.Text += "8"
+    End Sub
+
+    Private Sub btnNum9_Click(sender As Object, e As EventArgs) Handles btnNum9.Click
+        tbTextArea.Text += "9"
+    End Sub
+
+    Private Sub btnNum0_Click(sender As Object, e As EventArgs) Handles btnNum0.Click
+        tbTextArea.Text += "0"
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        tbTextArea.Clear()
+    End Sub
+
+    Private Sub btnAddition_Click(sender As Object, e As EventArgs) Handles btnAddition.Click
+        userOperator = "+"
+        initialValue = tbTextArea.Text
+        tbTextArea.Clear()
+    End Sub
+
+    Private Sub btnSubtraction_Click(sender As Object, e As EventArgs) Handles btnSubtraction.Click
+        userOperator = "-"
+        initialValue = tbTextArea.Text
+        tbTextArea.Clear()
+    End Sub
+
+    Private Sub btnMultiplication_Click(sender As Object, e As EventArgs) Handles btnMultiplication.Click
+        userOperator = "*"
+        initialValue = tbTextArea.Text
+        tbTextArea.Clear()
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        userOperator = "/"
+        initialValue = tbTextArea.Text
+        tbTextArea.Clear()
+    End Sub
+
+    Private Sub btnAnswer_Click(sender As Object, e As EventArgs) Handles btnAnswer.Click
+        Dim finalAnswer As Decimal
+        If (userOperator = "+") Then finalAnswer = Convert.ToInt32(initialValue) + Convert.ToInt32(tbTextArea.Text)
+        If (userOperator = "-") Then finalAnswer = Convert.ToInt32(initialValue) - Convert.ToInt32(tbTextArea.Text)
+        If (userOperator = "*") Then finalAnswer = Convert.ToInt32(initialValue) * Convert.ToInt32(tbTextArea.Text)
+        If (userOperator = "/") Then finalAnswer = Convert.ToInt32(initialValue) / Convert.ToInt32(tbTextArea.Text)
+        tbTextArea.Text = finalAnswer
+    End Sub
+End Class
+
